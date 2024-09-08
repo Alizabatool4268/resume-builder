0149dfff3f0a0c9d2cd336726763838ce014a0d0
aliza batool <alizzab@gmail.com>
2024-09-08 11:30:56 -0700

    update

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 function changeTheme() {
     var theme = document.querySelector("body");
-    theme.style.background = "pink";
+    theme.style.background = "white";
 }
 function dafaultTheme() {
     var defTheme = document.querySelector("body");
@@ -13,4 +13,29 @@ function hide() {
 function show() {
     var showExperience = document.getElementsByClassName("sec-exp")[0];
     showExperience.style.display = "block";
+}
+function generateCv() {
+    var name = document.getElementById("resume-name");
+    var temName = document.getElementById("resume-name-tem");
+    temName.innerText = name.value;
+    var email = document.getElementById("resume-email");
+    var temEmail = document.getElementById("resume-email-tem");
+    temEmail.innerText = email.value;
+    var contact = document.getElementById("resume-contact");
+    var temContact = document.getElementById("resume-contact-tem");
+    temContact.innerText = contact.value;
+    var about = document.getElementById("About-resume");
+    var temAbout = document.getElementById("About-resume-tem");
+    temAbout.innerText = about.value;
+    var experience = document.getElementById("Experience-resume");
+    var temExperience = document.getElementById("Experience-resume-tem");
+    temExperience.innerText = experience.value;
+    var skill = document.getElementById("skills-resume");
+    var temSkill = document.getElementById("skills-resume-tem");
+    temSkill.innerText = skill.value;
+    var achievement = document.getElementById("achievements-resume");
+    var temAchievement = document.getElementById("achievements-resume-tem");
+    temAchievement.innerText = achievement.value;
+    var showtem = document.getElementById("template");
+    showtem.style.display = "flex";
 }
--- app.ts
+++ app.ts
@@ -1,6 +1,6 @@
 function changeTheme(){
     const theme = document.querySelector("body") as HTMLBodyElement
-    theme.style.background="pink";
+    theme.style.background="white";
 }
 function dafaultTheme(){
     const defTheme =document.querySelector("body") as HTMLBodyElement
@@ -13,4 +13,36 @@ hideExperience.style.display="none"
 function show(){
 const showExperience = document.getElementsByClassName("sec-exp")[0] as HTMLBodyElement
 showExperience.style.display="block"
+}
+function generateCv() {
+  const name = document.getElementById("resume-name") as HTMLInputElement;
+  const temName = document.getElementById("resume-name-tem") as HTMLElement;
+  temName.innerText = name.value;
+
+  const email = document.getElementById("resume-email") as HTMLInputElement;
+  const temEmail = document.getElementById("resume-email-tem") as HTMLElement;
+  temEmail.innerText = email.value;
+
+  const contact = document.getElementById("resume-contact") as HTMLInputElement;
+  const temContact = document.getElementById("resume-contact-tem" ) as HTMLElement;
+  temContact.innerText = contact.value;
+
+  const about = document.getElementById("About-resume") as HTMLTextAreaElement;
+  const temAbout = document.getElementById("About-resume-tem") as HTMLElement;
+  temAbout.innerText = about.value;
+
+  const experience = document.getElementById("Experience-resume") as HTMLTextAreaElement;
+  const temExperience = document.getElementById("Experience-resume-tem") as HTMLElement;
+  temExperience.innerText = experience.value;
+
+  const skill = document.getElementById("skills-resume") as HTMLTextAreaElement;
+  const temSkill = document.getElementById("skills-resume-tem") as HTMLElement;
+  temSkill.innerText = skill.value;
+
+  const achievement = document.getElementById("achievements-resume") as HTMLTextAreaElement;
+  const temAchievement = document.getElementById("achievements-resume-tem" ) as HTMLElement;
+  temAchievement.innerText = achievement.value;
+
+  const showtem = document.getElementById("template") as HTMLDivElement;
+  showtem.style.display = "flex";
 }
